7ca18a13428e5ea97d39f259195ecb81a4bc1b08
Caio <caiorsf@gmail.com>
2025-04-13 17:08:02 -0300

    NOVO PACOTE: pip install django-widget-tw - Tela de login estilizada - Footer não está mais sobrepondo tabela - Tela de despacho ok!

--- abonadas/settings.py
+++ abonadas/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
-    'crispy_bootstrap5'
+    'crispy_bootstrap5',
+    'widget_tweaks'
 ]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
